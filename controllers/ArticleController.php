696caedf9d60112174ec7ee0d31c40dc558e6b4b
Kirill Nenashev <famiconbit@yahoo.com>
2019-11-24 15:41:11 +0700

    add models, view, controller for rbc

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -52,39 +52,4 @@ class ArticleController extends Controller
         return $this->render('article_view', ['model' => $model]);
     }
 
-
-    public function actionArticleProcess()
-    {
-        $list_items = Yii::$container->get(ArticleListInterface::class)->getArticleList();
-        foreach ($list_items as $article_item) {
-            $this->saveArticleItem($article_item);
-        }
-    }
-
-    /**
-     * @param ArticleItem $article
-     */
-    public function saveArticleItem(ArticleItem $article)
-    {
-        $article_model = new Article;
-        $article_model->title = $article->title;
-        $article_model->small_content = mb_substr($article->content, 0, 200);
-        $article_model->overview = $article->overview;
-        $article_model->content = $article->content;
-        $article_model->save();
-
-        if ($article->main_image) {
-            $article_image = new ArticleImage();
-            $article_image->src = $article->main_image;
-            $article_image->link('article', $article_model);
-        }
-
-        if ($article->original_page) {
-            $article_content = new ArticlePageContent();
-            $article_content->content = $article->original_page;
-            $article_content->original_link = $article->page_url;
-            $article_content->link('article', $article_model);
-        }
-    }
-
 }
